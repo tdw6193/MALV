6d2b7f3bff2e335f9f6d915016c6e0b2d131775b
Tai Wu <tdw6193@gmail.com>
2015-12-21 16:02:03 -0600

    Finished ApplyFragment

--- app/src/main/java/com/missasianlasvegas/twu/malv/ApplyFragment.java
+++ app/src/main/java/com/missasianlasvegas/twu/malv/ApplyFragment.java
@@ -1,11 +1,14 @@
 package com.missasianlasvegas.twu.malv;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 /**
  * Created by tdw6193 on 12/20/2015.
@@ -13,6 +16,8 @@ import android.view.ViewGroup;
 public class ApplyFragment extends Fragment{
 
     private Context context;
+
+    private ImageButton apply_btn;
 
     public ApplyFragment() {
     }
@@ -25,6 +30,22 @@ public class ApplyFragment extends Fragment{
 
         context = inflater.getContext();
 
+        apply_btn = (ImageButton) v.findViewById(R.id.apply_btn);
+        apply_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startApplying();
+            }
+        });
+
         return v;
+    }
+
+    private void startApplying() {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+        intent.setData(Uri.parse("http://missasianlasvegas.com/how-to-apply/"));
+        startActivity(intent);
     }
 }
