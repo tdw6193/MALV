e529157e262991999a9d4c1598718695dd5e6a2a
Tai Wu <tdw6193@gmail.com>
2015-12-21 15:23:05 -0600

    Finished ContestantsFragment and ContestantInfoFragment, except for flipping card animations

---
+++ app/src/main/java/com/missasianlasvegas/twu/malv/ContestantInfoFragment.java
@@ -0,0 +1,67 @@
+package com.missasianlasvegas.twu.malv;
+
+import android.support.v4.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+/**
+ * Created by tdw6193 on 12/21/2015.
+ */
+public class ContestantInfoFragment extends Fragment {
+
+    private Context context;
+
+    private String cName, cCountry, cDesc;
+
+    private TextView contestant_name, contestant_country, contestant_desc;
+    private ImageButton flip_btn;
+
+    public ContestantInfoFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_contestant_info, container, false);
+
+        context = inflater.getContext();
+
+        Bundle bundle = this.getArguments();
+        if(bundle != null) {
+            cName = bundle.getString("NAME", "Problem loading contestant's name");
+            cCountry = bundle.getString("COUNTRY", "Problem loading contestant's country");
+            cDesc = bundle.getString("DESC", "Problem loading contestant's description");
+
+            contestant_name = (TextView) v.findViewById(R.id.contestant_name);
+            contestant_country = (TextView) v.findViewById(R.id.contestant_country);
+            contestant_desc = (TextView) v.findViewById(R.id.contestant_desc);
+
+            contestant_name.setText(cName);
+            contestant_country.setText(cCountry);
+            contestant_desc.setText(cDesc);
+        }
+
+        flip_btn = (ImageButton) v.findViewById(R.id.contestant_flip_btn);
+        flip_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        //.setCustomAnimations(R.animator.card_flip_right_in, R.animator.card_flip_right_out,
+                        //                     R.animator.card_flip_left_in, R.animator.card_flip_left_out)
+                        .setCustomAnimations(android.R.anim.slide_in_left, android.R.anim.slide_out_right)
+                        .replace(R.id.mainContent, new ContestantsFragment())
+                        .addToBackStack(null)
+                                        .commit();
+            }
+        });
+
+        return v;
+    }
+}
--- app/src/main/java/com/missasianlasvegas/twu/malv/ContestantsFragment.java
+++ app/src/main/java/com/missasianlasvegas/twu/malv/ContestantsFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 /**
  * Created by tdw6193 on 12/20/2015.
@@ -13,6 +14,11 @@ import android.view.ViewGroup;
 public class ContestantsFragment extends Fragment {
 
     private Context context;
+
+    private ImageButton arroyoBtn, bajracharyaBtn, bayaniBtn;
+    private ImageButton camachoBtn, angelesBtn, johnsonBtn;
+    private ImageButton lankfordBtn, liuBtn, shoafBtn;
+    private ImageButton techBtn, vibasBtn, zeiglerBtn;
 
     public ContestantsFragment() {
     }
@@ -25,6 +31,134 @@ public class ContestantsFragment extends Fragment {
 
         context = inflater.getContext();
 
+        arroyoBtn = (ImageButton) v.findViewById(R.id.arroyo_img);
+        bajracharyaBtn = (ImageButton) v.findViewById(R.id.bajracharya_img);
+        bayaniBtn = (ImageButton) v.findViewById(R.id.bayani_img);
+        camachoBtn = (ImageButton) v.findViewById(R.id.camacho_img);
+        angelesBtn = (ImageButton) v.findViewById(R.id.angeles_img);
+        johnsonBtn = (ImageButton) v.findViewById(R.id.johnson_img);
+        lankfordBtn = (ImageButton) v.findViewById(R.id.lankford_img);
+        liuBtn = (ImageButton) v.findViewById(R.id.liu_img);
+        shoafBtn = (ImageButton) v.findViewById(R.id.shoaf_img);
+        techBtn = (ImageButton) v.findViewById(R.id.tech_img);
+        vibasBtn = (ImageButton) v.findViewById(R.id.vibas_img);
+        zeiglerBtn = (ImageButton) v.findViewById(R.id.zeigler_img);
+
+        arroyoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flipFragment("Renea Arroyo",
+                             "Countries: China and Thailand",
+                             "Top 5");
+            }
+        });
+        bajracharyaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flipFragment("Esha Bajracharya",
+                             "Country: Nepal",
+                             "Miss Social Media");
+            }
+        });
+        bayaniBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flipFragment("Ami Bayani",
+                             "Country: Philippines",
+                             "2015 Miss Asian Las Vegas Winner");
+            }
+        });
+        camachoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flipFragment("Catalina Camacho",
+                             "Country: Philippines",
+                             "Miss Congeniality");
+            }
+        });
+        angelesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flipFragment("Renee Delos Angeles",
+                             "Country: Philippines",
+                             "");
+            }
+        });
+        johnsonBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flipFragment("Shelby Kanani Johnson",
+                             "Country: Japan",
+                             "2015 Miss Asian Las Vegas, 1st Runner Up");
+            }
+        });
+        lankfordBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flipFragment("Amabell Lankford",
+                             "Country: South Korea",
+                             "Miss Talent");
+            }
+        });
+        liuBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flipFragment("Ashley Liu",
+                             "Country: China",
+                             "Miss Modern Asian/Entrepreneur\n" +
+                             "Miss People’s Choice\n" +
+                             "2015 Miss Asian Las Vegas, 2nd Runner Up");
+            }
+        });
+        shoafBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flipFragment("Daphnie Shoaf",
+                             "Country: Philippines",
+                             "Best Costume");
+            }
+        });
+        techBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flipFragment("Daphnie Shoaf",
+                        "Country: Cambodia",
+                        "Top 5");
+            }
+        });
+        vibasBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flipFragment("Joanna Vibas",
+                             "Country: Philippines",
+                             "");
+            }
+        });
+        zeiglerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flipFragment("Manee Zeigler",
+                             "Country: Thailand",
+                             "");
+            }
+        });
+
         return v;
+    }
+
+    private void flipFragment(String cName, String cCountry, String cDesc) {
+        ContestantInfoFragment contestantInfoFragment = new ContestantInfoFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString("NAME", cName);
+        bundle.putString("COUNTRY", cCountry);
+        bundle.putString("DESC", cDesc);
+        contestantInfoFragment.setArguments(bundle);
+
+        getFragmentManager()
+                .beginTransaction()
+                .setCustomAnimations(android.R.anim.slide_in_left, android.R.anim.slide_out_right)
+                .replace(R.id.mainContent, contestantInfoFragment)
+                .addToBackStack(null)
+                .commit();
     }
 }
